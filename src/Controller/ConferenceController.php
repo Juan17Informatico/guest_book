a8946d6e608551f15cbfa0e5012d2685ff42a887
Juan17Informatico <juancampuzano2356@gmail.com>
2022-03-31 23:18:20 -0500

    paso 10 creando pagina para mostrar las conferencias

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +13,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index($name)
+    public function index(ConferenceRepository $conferenceRepository)
     {
         
-        return $this->render('conference/index.html.twig');
+        $conferences = $conferenceRepository->findAll();
+        return $this->render('conference/index.html.twig',[
+            'conferences' => $conferences
+        ]);
 
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(CommentRepository $commentRepository, Conference $conference)
+    {
+        $comments = $commentRepository->findBy(
+            ['conference' => $conference],
+            ['createdAt' => 'DESC']
+        );
+        return $this->render('conference/show.html.twig',[
+            'conference' => $conference,
+            'comments' => $comments
+        ]);
     }
 }
